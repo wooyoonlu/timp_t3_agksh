3b3f9e663cc6cc149c710dc06d638634f72e71ec
green_owl <hello.mgololobov@gmail.com>
2026-06-02 22:48:18 +0300

    Update mytcpserver.h

--- mytcpserver.h
+++ mytcpserver.h
@@ -6,3 +6,26 @@
 #include <QTcpSocket>
 #include <QList>
 #include <QMap>
+
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit MyTcpServer(QObject *parent = nullptr);
+    ~MyTcpServer();
+
+public slots:
+    void slotNewConnection();
+    void slotClientDisconnected();
+    void slotServerRead();
+
+    QString handleRequest(const QString& request, QTcpSocket* client);
+
+private:
+    QTcpServer *mTcpServer;
+    QList<QTcpSocket*> m_clients;               // все подключенные клиенты
+    QMap<QTcpSocket*, QString> m_sessions;     // сокет -> логин авторизованного пользователя
+};
+
+#endif // MYTCPSERVER_H
