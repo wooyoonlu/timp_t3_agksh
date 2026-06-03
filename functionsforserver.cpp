17f121655060b4c9d8800142bd0389f6d24fa9a5
green_owl <hello.mgololobov@gmail.com>
2026-06-03 08:07:12 +0300

    Create functionsforserver.cpp

---
+++ functionsforserver.cpp
@@ -0,0 +1,17 @@
+#include "functionsforserver.h"
+
+QString vigenereEncrypt(const QString& text, const QString& key) {
+    return "ENCRYPT_STUB";
+}
+QString vigenereDecrypt(const QString& cipher, const QString& key) {
+    return "DECRYPT_STUB";
+}
+QString sha512(const QString& input) {
+    return "HASH_STUB";
+}
+double bisection(double a, double b, double eps) {
+    return 0.0;
+}
+int shortestPath(const QString& graphDescription, int start, int end) {
+    return -1;
+}
